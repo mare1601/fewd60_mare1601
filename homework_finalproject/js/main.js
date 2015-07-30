29597a09388196f6718dc42ebf1422390b959499
mare1601 <mary.e.ellis@gmail.com>
2015-07-30 00:39:04 -0400

    More updates
    lalala

--- homework_finalproject/js/main.js
+++ homework_finalproject/js/main.js
@@ -14,10 +14,10 @@ if ($(this).scrollTop() > 5){
 });
 
 $(window).scroll(function() {
-if ($(this).scrollTop() > 400){  
+if ($(this).scrollTop() > 1000){  
     $('.plane').addClass('fly');
   }
   else{
-    $('.plane').removeClass('fly');
+    $('.plane').removeClass('fly'), $('.plane').addClass('hidden');
   }
 });
